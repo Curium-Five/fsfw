a9c6c088f25adf1b4b8fca1736276e5e5fe227ba
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-11-14 14:08:38 +0100

    remove leftover debugging code

--- src/fsfw/globalfunctions/timevalOperations.cpp
+++ src/fsfw/globalfunctions/timevalOperations.cpp
@@ -1,12 +1,9 @@
 #include "fsfw/globalfunctions/timevalOperations.h"
 
-#include <cstdio>
-
 timeval& operator+=(timeval& lhs, const timeval& rhs) {
   int64_t sum = static_cast<int64_t>(lhs.tv_sec) * 1000000. + lhs.tv_usec;
   sum += static_cast<int64_t>(rhs.tv_sec) * 1000000. + rhs.tv_usec;
-  int64_t tmp = sum / 1000000;
-  lhs.tv_sec = tmp;
+  lhs.tv_sec = sum / 1000000;
   lhs.tv_usec = sum - lhs.tv_sec * 1000000;
   return lhs;
 }
